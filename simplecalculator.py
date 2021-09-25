c8503f23106ad5920cd51158b2e5bc4bc85759cc
Tama <86469864+melonpan05@users.noreply.github.com>
2021-09-25 13:47:19 +0700

    Add files via upload

--- simplecalculator.py
+++ simplecalculator.py
@@ -4,6 +4,128 @@ from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import pyqtSlot
 from PyQt5 import QtCore
 from PyQt5.QtGui import * 
+
+class app_2(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.ui()
+     
+    def ui(self):
+        self.setGeometry(10 , 10 , 234 , 338)
+        self.setWindowTitle("Simple calculator")
+        self.setStyleSheet("background-color:#384147;color:white")
+        button1 = QPushButton('1' , self)
+        button2 = QPushButton('2' , self)
+        button3 = QPushButton('3' , self)
+        button4 = QPushButton('4' , self)
+        button5 = QPushButton('5' , self)
+        button6 = QPushButton('6' , self)
+        button7 = QPushButton('7' , self)
+        button8 = QPushButton('8' , self)
+        button9 = QPushButton('9' , self)
+        button0 = QPushButton('0' , self)
+        buttonplus = QPushButton('+' , self)
+        buttonminus = QPushButton('-' , self)
+        buttonx = QPushButton('x' , self)
+        button_ = QPushButton('/' , self)
+        buttonresult = QPushButton('=' , self)
+        buttondot = QPushButton("." , self)
+        buttonclear = QPushButton("C" , self)
+        button1.resize(55 , 55)
+        button2.resize(55 , 55)
+        button3.resize(55 , 55)
+        button4.resize(55 , 55)
+        button5.resize(55 , 55)
+        button6.resize(55 , 55)
+        button7.resize(55 , 55)
+        button8.resize(55 , 55)
+        button9.resize(55 , 55)
+        button0.resize(55 , 55)
+        buttonplus.resize(55 , 55)
+        buttonx.resize(55 , 55)
+        button_.resize(55 , 55)
+        buttonresult.resize(55 , 55)
+        buttondot.resize(55 , 55)
+        buttonclear.resize(55 , 55)
+        buttonminus.resize(55 , 55)
+        self.textbox1 = QLineEdit(self)
+        self.textbox1.resize(220 , 55)
+        self.textbox1.setStyleSheet("font-size:25px;background-color:white;color:black")
+        self.textbox1.move(7,1)
+        button1.clicked.connect(lambda:self.add_text('1'))
+        button2.clicked.connect(lambda:self.add_text('2'))
+        button3.clicked.connect(lambda:self.add_text('3'))
+        button4.clicked.connect(lambda:self.add_text('4'))
+        button5.clicked.connect(lambda:self.add_text('5'))
+        button6.clicked.connect(lambda:self.add_text('6'))
+        button7.clicked.connect(lambda:self.add_text('7'))
+        button8.clicked.connect(lambda:self.add_text('8'))
+        button9.clicked.connect(lambda:self.add_text('9'))
+        button0.clicked.connect(lambda:self.add_text('0'))
+        buttonplus.clicked.connect(lambda:self.add_text('+'))
+        buttonminus.clicked.connect(lambda:self.add_text('-'))
+        buttonx.clicked.connect(lambda:self.add_text('*'))
+        button_.clicked.connect(lambda:self.add_text('/'))
+        buttondot.clicked.connect(lambda:self.add_text('.'))
+        buttonclear.clicked.connect(self.clear)
+        buttonresult.clicked.connect(self.result)
+        x = 7
+        y = 55
+        button1.move(x , y)
+        x += 55
+        button2.move(x, y)
+        x += 55
+        button3.move(x , y)
+        x += 55
+        button4.move(x , y)
+        x = 7
+        y += 55
+        button5.move(x , y)
+        x += 55
+        button6.move(x , y)
+        x += 55
+        button7.move(x , y)
+        x += 55
+        button8.move(x , y)
+        x = 7
+        y += 55
+        button9.move(x , y)
+        x += 55
+        button0.move(x , y)
+        x += 55
+        buttonplus.move(x , y)
+        x += 55
+        buttonminus.move(x , y)
+        x = 7
+        y += 55
+        buttonx.move(x , y)
+        x += 55
+        button_.move(x , y)
+        x += 55
+        buttondot.move(x , y)
+        x += 55
+        buttonresult.move(x , y)
+        x = 7
+        y += 55
+        buttonclear.move(x , y)
+
+    def add_text(self , x):
+        text = self.textbox1.text()
+        self.textbox1.setText(str(text) + str(x))
+
+    def clear(self):
+        self.textbox1.setText("")
+
+    def result(self):
+        text = self.textbox1.text()
+        result = 0
+        try:
+            result = eval(text)
+        except ZeroDivisionError:
+            result = 0
+        self.textbox1.setText(str(result))
+
 class App(QWidget):
 
     def __init__(self):
@@ -19,10 +141,10 @@ class App(QWidget):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
         self.setStyleSheet("background-color:#384147;color:white")
-        label1 = QLabel("Angka pertama" , self)
+        label1 = QLabel("First number : " , self)
         label1.setStyleSheet("font-family:'Arial';")
         label1.resize(100 , 30)
-        label2 = QLabel("Angka kedua" , self)
+        label2 = QLabel("Second number : " , self)
         label2.resize(100 , 30)
         self.result = QLineEdit(self)
         self.result.resize(100,30)
@@ -65,8 +187,31 @@ class App(QWidget):
         result = eval(result)
         self.result.setText(str(result))
         
+class main_menu(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Simple calculator")
+        self.setStyleSheet("background-color:#384147;color:white")
+        self.setGeometry(10 , 10 , 250 , 150)
+        self.button1 = QPushButton("Basic mode" , self)
+        self.button1.resize(200 , 50)
+        self.button2 = QPushButton("Advanced mode" , self)
+        self.button2.resize(200 , 50)
+        self.button1.move(10 , 10)
+        self.button2.move(10 , 60)
+        self.button1.clicked.connect(lambda:self.show1(1))
+        self.button2.clicked.connect(lambda:self.show1(2))
+
+    def show1(self , x):
+        if x == 1:
+            ex = App()
+            ex.show()
+        elif x == 2:
+            ex = app_2()
+            ex.show()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = App()
+    ex = main_menu()
+    ex.show()
     sys.exit(app.exec_())
